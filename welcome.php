c30e1a0cf51e908ef7fc6e4b25c03bfb86eeb897
Gayle DiCarlantonio <gayledmail@gmail.com>
2018-11-29 19:17:05 -0800

    initial commit

---
+++ welcome.php
@@ -0,0 +1,37 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: index.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; text-align: center; 
+        background-image: url("assets/Spaceanimation.gif"); }
+    </style>
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Prepare to die!!</h1>
+    </div>
+    <p>
+        <a href="game.html" class="btn btn-success">Start Space Shooter Game</a>
+    </p>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>    
+        
+    
+</body>
+</html>
